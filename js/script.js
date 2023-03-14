6ec5ff7bca974b36b322dc000366199aecd3c309
anashamimi <anashamimi2@gmail.com>
2023-03-14 01:07:21 +0100

    aggiunta gioco dadi

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ const mail = [
     'carlomagno76@gmail.com', 'lautaromartinez10@gmail.com'
 ];
 
-const btn = document.querySelector('button');
+const btnMail = document.querySelector('button');
 
-btn.addEventListener('click', function checklist(e) {
+btnMail.addEventListener('click', function checklist(e) {
     e.preventDefault();
     let accesso = '';
     const searchMail = document.getElementById('user-mail').value;
@@ -29,10 +29,49 @@ btn.addEventListener('click', function checklist(e) {
     if (accesso) {
         accesso = 'Benvenuto!!!'
         document.getElementById('app').innerHTML += `<p>${accesso}</p>`;
-        console.log('Benvenuto');
+        console.log(accesso);
     } else {
         accesso = 'Non sei nella lista!!!'
         document.getElementById('app').innerHTML += `<p class="text-danger">${accesso}</p>`;
-        console.log('Non sei nella lista');
+        console.log(accesso);
     }
+});
+
+
+const btnDado = document.getElementById('btn-dadi');
+
+btnDado.addEventListener('click', function playdice(e) {
+    e.preventDefault();
+    let esito = '';
+
+    let numDadoComputer1 = getRndNumber(1 , 6);
+    let numDadoComputer2 = getRndNumber(1 , 6);
+    let numDadoUser1 = getRndNumber(1 , 6);
+    let numDadoUser2 = getRndNumber(1 , 6);
+
+    let sumComputer = numDadoComputer1 + numDadoComputer2;
+
+    let sumUser = numDadoUser1 + numDadoUser2;
+
+    console.log(sumComputer);
+
+    console.log(sumUser);
+    
+
+    if (sumComputer > sumUser) {
+        esito = 'Mi dispiace ma il computer è troppo forte :(';
+    } else if (sumComputer < sumUser) {
+        esito = 'Congratulazioni hai battuto il computer :)';
+    } else if (sumComputer == sumUser) {
+        esito = 'è un pareggio la prossima volta andrà meglio :|'
+    }
+    console.log(esito);
+
+    document.getElementById('dadi-computer').innerHTML += `<div class="num-dadi d-flex align-items-center justify-content-center">${numDadoComputer1}</div>
+    <div class="num-dadi d-flex align-items-center justify-content-center">${numDadoComputer2}</div>`;
+
+    document.getElementById('dadi-user').innerHTML += `<div class="num-dadi d-flex align-items-center justify-content-center">${numDadoUser1}</div>
+    <div class="num-dadi d-flex align-items-center justify-content-center">${numDadoUser2}</div>`;
+
+    document.getElementById('dadi').innerHTML += `<p class="text-center text-warning p-5">${esito}</p>`;
 });
